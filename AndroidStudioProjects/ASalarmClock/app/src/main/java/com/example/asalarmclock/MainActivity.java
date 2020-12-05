2ce922574a453ea2c525b2f3a3e83f72f8232e23
AKS0606 <sakochi987987@gmail.com>
2020-12-05 14:54:45 +0900

    test

---
+++ AndroidStudioProjects/ASalarmClock/app/src/main/java/com/example/asalarmclock/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.asalarmclock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * ↓のように引数を省略すると"Hello World!"が表示されない。
+     * protected void onCreate() {
+     *         setContentView(R.layout.activity_main);
+     *     }
+     * @param savedInstanceState
+     */
+      @Override
+          protected void onCreate(Bundle savedInstanceState) {
+               super.onCreate(savedInstanceState);
+               setContentView(R.layout.activity_main);
+          }
+
+
+}
